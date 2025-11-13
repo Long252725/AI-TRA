9a5ba20d3abcbf3e5713cfa796dcc266e5729ffe
Long252725 <inthanhlong2006@gmail.com>
2025-11-13 22:41:36 +0700

    a

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Set up multer để xử lý file upload
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: "/tmp/uploads/" });
 
 // Template engine
 app.engine('handlebars', engine({
